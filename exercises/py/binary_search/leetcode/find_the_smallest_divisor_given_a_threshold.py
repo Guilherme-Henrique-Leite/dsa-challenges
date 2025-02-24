9685cc53ef1fc7c2074c997c0f61ecf32b861ac2
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2025-02-23 23:00:16 -0300

    add: Add variables to smallestdivisor

--- exercises/py/binary_search/leetcode/find_the_smallest_divisor_given_a_threshold.py
+++ exercises/py/binary_search/leetcode/find_the_smallest_divisor_given_a_threshold.py
@@ -1,5 +1,6 @@
 # 1283. Find the Smallest Divisor Given a Threshold
 
+nums = [1,2,5,9], threshold = 6
 def smallestDivisor(nums, threshold):
     def sum_calculate(divisor):
         return sum((num + divisor - 1) // divisor for num in nums)
@@ -13,3 +14,5 @@ def smallestDivisor(nums, threshold):
         else:
             left = mid + 1
     return left 
+
+smallestDivisor(nums=nums, threshold=threshold)
